f72de0e98896d194ec7d9184533ec180dfd752f2
ackyes <61712293+ackyes@users.noreply.github.com>
2021-10-26 22:28:15 +0000

    main branch

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print ("hello world")
+import requests
+
+url_host = "https://dog.ceo"
+endpoint = "/api/breeds/list/all"
+query = ""
+
+url = url_host + endpoint # + query
+
+response = requests.get(url)
+print(response.json())

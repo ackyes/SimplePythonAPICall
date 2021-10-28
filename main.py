76c40116e7a1158500fae7c1ef94b660e309bfd7
ackyes <61712293+ackyes@users.noreply.github.com>
2021-10-28 21:56:01 +0000

    prettifying display of dog breeds

--- main.py
+++ main.py
@@ -7,4 +7,17 @@ query = ""
 url = url_host + endpoint # + query
 
 response = requests.get(url)
-print(response.json())
+breeds = response.json()["message"]
+
+#get a list of all breeds from API call
+all_breeds = breeds.keys()
+
+# loop and print out each key
+for breed in all_breeds:
+  print(breed)
+
+# displays all poodles subbreeds
+poodle_subbreeds = breeds["poodle"]
+input("Press enter to get all poodle subreeds")
+for sub in poodle_subbreeds:
+  print(sub)
